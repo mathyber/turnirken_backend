074dec9abb06b080a93c1b77638b8e33add89c45
mathyber <ilan23f@gmail.com>
2020-05-29 23:30:30 +0300

    +

--- src/main/java/com/example/turnirken/entity/AppUser.java
+++ src/main/java/com/example/turnirken/entity/AppUser.java
@@ -2,6 +2,7 @@ package com.example.turnirken.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.NotFound;
 
 import javax.persistence.*;
 import java.util.HashSet;
